9a37b1bda92a2f2940a442470cee14cf94506dfe
shailgautam4ever <rayskcoc@gmail.com>
2023-10-27 22:27:33 +0530

    Added Location Mock

---
+++ src/mockdata/locations.js
@@ -0,0 +1,39 @@
+
+export const popularCities = [
+    {
+        name:"Delhi",
+        icon:"/assets/delhi.png"
+    },
+    {
+        name:"Gurgaon",
+        icon:"/assets/gurgaon.png"
+    },
+    {
+        name:"Noida",
+        icon:"/assets/noida.png"
+    },
+    {
+        name:"Bangalore",
+        icon:"/assets/bangalore.png"
+    },
+    {
+        name:"Hyderabad",
+        icon:"/assets/hyderabad.png"
+    },
+    {
+        name:"Mumbai",
+        icon:"/assets/mumbai.png"
+    }
+]
+
+export const otherCities = [
+    "Ghaziabad","Kanpur", "Kolkata", "Jaipur", "Chennai", "Patna", "Agra", "Surat", "Lucknow", "Bulandshahr"
+    , "Amritsar", "Bhopal", "Panchkula", "Faridabad", "Mohali", "Pune", "Dehradun", "Ahemdabad", "Panipat"
+    , "Karnal", "Greater Noida", "Chandigarh", "Ludhiana", "Jalandhar", "Ranchi", "Rohtak", "Rudrapur", "Haridwar",
+    "Vijayawada", "Bareilly", "Goa", "Mathura", "Kashmir"
+]
+
+export default {
+    popularCities,
+    otherCities
+}
