735dce9adc49d5dcc3e78336978cb9495174ff45
shailgautam4ever <rayskcoc@gmail.com>
2023-10-27 22:28:53 +0530

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+const Footer = ()=>{
+    return(
+        <FooterContainer>Footer</FooterContainer>
+    )
+}
+
+export default Footer;
+
+const FooterContainer = styled.div`
+    background-color: yellow;
+`
