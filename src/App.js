ed332c9b6b618a25ee2931585d7f32d769ec5a4a
shailgautam4ever <rayskcoc@gmail.com>
2023-10-27 22:31:06 +0530

    Set up app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ContentManager from "./components/ContentManager";
+import Swiper from "./components/Swiper";
+import SwiperSlider from "./components/Swiper";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainContainer>
+        <Navbar/>
+        <ContentManager/>
+        <Footer/>
+      </MainContainer>
+    </>
   );
-}
+};
 
 export default App;
+
+const MainContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+`;
