26fac9172e4004df1162c6a29cefc005ae0dfe8d
shailgautam4ever <rayskcoc@gmail.com>
2023-11-05 21:57:32 +0530

    Added Feature comp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,25 @@
 import Swiper from "../components/Swiper"
-
+import FeatureCake from "../components/FeatureCake";
+import { styled } from "styled-components";
 
 const Home = ()=>{
-    return(
-        <Swiper></Swiper>
+    return(<>
+        <Swiper/>
+        <Line/>
+        <FeatureCake/>
+        </>
+
     )
 }
 
 export default Home;
+
+const Line = styled.hr`
+    /* width: 365px; */
+    width: 90%;
+    margin-top: 30px;
+    height: 1px;
+    background-color: #eee;
+    color: #eee;
+    border: none;
+`
