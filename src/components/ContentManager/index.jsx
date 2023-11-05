012a2c28d9221d47e87ddc5ed2dbb99a0689b2fb
shailgautam4ever <rayskcoc@gmail.com>
2023-11-05 22:01:37 +0530

    added route to cake page

--- src/components/ContentManager/index.jsx
+++ src/components/ContentManager/index.jsx
@@ -3,13 +3,14 @@ import Swiper from "../Swiper";
 import { Route, Routes } from "react-router";
 import Footer from "../Footer";
 import Home from "../../pages/home";
+import Cakes from "../../pages/cakes";
 
 const ContentManager = ()=>{
     return (
         <Container>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
-                <Route path="/cake" element={<Footer/>} ></Route>
+                <Route path="/cakes" element={<Cakes/>} ></Route>
             </Routes>
             
         </Container>
