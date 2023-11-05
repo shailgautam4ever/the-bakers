3276f4690b8a55401c9784e79c1f061c0595e572
shailgautam4ever <rayskcoc@gmail.com>
2023-11-05 21:57:03 +0530

    Added cake mock data

---
+++ src/mockdata/cake.js
@@ -0,0 +1,51 @@
+
+export const cake = [
+      {
+        name: "Chocolate Cake",
+        url: "/assets/cakeImages/RegCake.webp",
+      },
+      {
+        name: "Gourmet Fudge Cake",
+        url: "/assets/cakeImages/GC.webp",
+      },
+      {
+        name: "Fruitberry Cake",
+        url: "/assets/cakeImages/fruitCake.webp",
+      },
+      {
+        name: "Mango Cake",
+        url: "/assets/cakeImages/Theme.webp",
+      },
+      {
+        name: "Chocolate Cake",
+        url: "/assets/cakeImages/RegCake.webp",
+      },
+      {
+        name: "Gourmet Fudge Cake",
+        url: "/assets/cakeImages/GC.webp",
+      },
+      {
+        name: "Fruitberry Cake",
+        url: "/assets/cakeImages/fruitCake.webp",
+      },
+      {
+        name: "Mango Cake",
+        url: "/assets/cakeImages/Theme.webp",
+      },
+      {
+        name: "Chocolate Cake",
+        url: "/assets/cakeImages/RegCake.webp",
+      },
+      {
+        name: "Gourmet Fudge Cake",
+        url: "/assets/cakeImages/GC.webp",
+      },
+      {
+        name: "Fruitberry Cake",
+        url: "/assets/cakeImages/fruitCake.webp",
+      },
+      {
+        name: "Mango Cake",
+        url: "/assets/cakeImages/Theme.webp",
+      }
+    ]
